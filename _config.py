0e0a281f37e59eea4725d2e64c5c6c1ea132fa5b
Joshua Cai <Joshua.p.cai.python@gmail.com>
2016-06-17 10:14:36 -0400

    commit chapter

---
+++ _config.py
@@ -0,0 +1,13 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'A8UFS0GOQf9g2UPGIUABP92Ppaiouf49yusagf8IU)*&F&T3po&^%E&$S&%RH'
+
+
+# define the full path for the db
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
