4eba221e5254aa4f53b8724bbf6dc173add87a6c
Joshua Cai <Joshua.p.cai.python@gmail.com>
2016-06-20 12:37:01 -0400

    switch to sqlalchmy

--- _config.py
+++ _config.py
@@ -1,4 +1,8 @@
+#project/_config.py
+
 import os
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
@@ -11,3 +15,5 @@ SECRET_KEY = 'A8UFS0GOQf9g2UPGIUABP92Ppaiouf49yusagf8IU)*&F&T3po&^%E&$S&%RH'
 # define the full path for the db
 DATABASE_PATH = os.path.join(basedir, DATABASE)
 
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
--- db_create.py
+++ db_create.py
@@ -2,20 +2,18 @@
 
 
 
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-    c = connection.cursor()
 
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
-    status INTEGER NOT NULL)""")
+# create the database and the db table
+db.create_all()
 
-    c.execute('INSERT INTO tasks (name, due_date, priority, status)'
-            'VALUES("Finish this tutorial", "03/25/2015", 10, 1)'
-            )
+# insert data
+db.session.add(Task("Finish this tutorial", date(2015, 3, 13), 10, 1))
 
-    c.execute('INSERT INTO tasks (name, due_date, priority, status)'
-            'VALUES("Finish Real Python Course 2", "03/25/2015", 10, 1)'
-            )
+db.session.add(Task("Finish realpython", date(2015, 3, 13), 10, 1))
+
+# commit the changes
+db.session.commit()
